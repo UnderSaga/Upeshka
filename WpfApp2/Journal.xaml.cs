be7f2c87051233bf9dc0f7c8ee429da9a7bb6a9b
UnderSaga <undersaga@mail.ru>
2023-03-08 20:41:15 +0300

    Написано, сделано, готово

---
+++ WpfApp2/Journal.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClassLibrary2;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Journal.xaml
+    /// </summary>
+    public partial class Journal : Window
+    {
+        public ObservableCollection<Grop> Grops = new ObservableCollection<Grop>();
+        public Journal()
+        {
+            InitializeComponent();
+            using (var db = new DBContext())
+            {
+                List<Grop> GropList = db.Grops.ToList();
+                foreach(Grop group in GropList)
+                {
+                    groups.Items.Add(group);
+                }
+            }    
+        }
+    }
+}
